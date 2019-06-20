9bbf06d02514d7589c5e5d635c7b30ff1be00497
Hashly <hashly.quevedo@hotmail.es>
2019-06-20 15:48:45 -0500

    Configuracion remota

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import web
 
-db_host = '127.0.0.1'
-db_name = 'kuorra_login'
-db_user = 'kuorra'
-db_pw = 'kuorra.2018'
+db_host = 'bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'h1kc7hdqez5ycwe8'
+db_user = 'gbtqgw92wg5jlsgr'
+db_pw = '	z1urnujodv62xscl'
 
 db = web.database(
     dbn='mysql',
